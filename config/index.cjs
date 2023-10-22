655764cca0c21f8e9417a9a07205dce5bee0f3dc
Cornf1ower <131224603+a67513901@users.noreply.github.com>
2023-10-22 23:25:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ USE_PASSAGE: 'push-plus',
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '35484fbb6d334e628988e931d7ece3bb',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,13 +51,13 @@ USE_PASSAGE: 'push-plus',
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '35484fbb6d334e628988e931d7ece3bb',
+      id: '',
     }
   ],
 
